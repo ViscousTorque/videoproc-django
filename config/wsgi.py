3cdd2402fbef40adcd83d5f2c04805691ecd9c86
ViscousTorque <viscoustorque@yahoo.co.uk>
2025-04-22 20:48:07 +0100

    Use gunicorn

---
+++ config/wsgi.py
@@ -0,0 +1,8 @@
+"""Standard WSGI"""
+import os
+from django.core.wsgi import get_wsgi_application
+
+# Set the default settings module for the 'django' program.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+application = get_wsgi_application()
