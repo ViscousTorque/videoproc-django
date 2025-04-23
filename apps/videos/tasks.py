c412afe7be1d8a70738bade8b423f1e80a4f090b
ViscousTorque <viscoustorque@yahoo.co.uk>
2025-04-23 13:58:32 +0100

    Add span attribute for input_file

--- apps/videos/tasks.py
+++ apps/videos/tasks.py
@@ -1,19 +1,21 @@
-from moviepy import *
-from django.utils import timezone
+"""Task for celery video processing"""
+import os
+from moviepy import VideoFileClip
 from opentelemetry import trace
 from celery import shared_task
-from apps.videos.models import Video
-from django.conf import settings
-import os
 
-# Initialize the tracer
+from django.utils import timezone
+from django.conf import settings
+
+from apps.videos.models import Video
+
 tracer = trace.get_tracer(__name__)
 
 tmp_dir = os.getenv('TMPDIR', settings.MEDIA_ROOT + '/tmp')
 
 @shared_task(queue='encoding')
 def process_video(video_id):
-    with tracer.start_as_current_span("task_process_video"):
+    with tracer.start_as_current_span("task_process_video") as input_span:
         try:
             video = Video.objects.get(id=video_id)
             input_path = os.path.join(settings.MEDIA_ROOT, video.file.name)
@@ -24,18 +26,21 @@ def process_video(video_id):
                 print(f"[WARNING] File too small to process: {input_path}")
                 return
             filename = os.path.basename(input_path)
+            file_size = os.path.getsize(input_path)
+            input_span.set_attribute("video.task_process_video.file_size", file_size)
+            input_span.set_attribute("video.task_process_video.file_path", input_path)
             output_filename = f"processed_{filename}"
             output_path = os.path.join(settings.MEDIA_ROOT, 'processed', output_filename)
             os.makedirs(os.path.dirname(output_path), exist_ok=True)
 
-            with tracer.start_as_current_span("resize_and_write") as span:
+            with tracer.start_as_current_span("resize_and_write") as output_span:
                 clip = VideoFileClip(input_path)
                 clip_resized = clip.resized(height=720)
                 clip_resized.write_videofile(output_path, codec='libx264', audio_codec='aac')
 
                 file_size = os.path.getsize(output_path)
-                span.set_attribute("video.resize_and_write.file_size", file_size)
-                span.set_attribute("video.resize_and_write.file_path", output_path)
+                output_span.set_attribute("video.resize_and_write.file_size", file_size)
+                output_span.set_attribute("video.resize_and_write.file_path", output_path)
 
             video.output_file.name = os.path.relpath(output_path, settings.MEDIA_ROOT)
             video.processed = True
@@ -47,10 +52,3 @@ def process_video(video_id):
             print(f"[ERROR] Video processing failed: {e}")
             video.status = 'failed'
             video.save()
-
-
-        except Exception as e:
-            print(f"[ERROR] Failed to process video {video_id}: {e}")
-            video.status = 'failed'
-            video.save()
-
